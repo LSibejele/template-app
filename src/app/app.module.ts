71b9941870441be4a48d385499db9d5ef3d442d7
Lonwabo Sibejele <lonwabo.sibejele@entelect.co.za>
2020-06-22 08:52:35 +0200

    navigation component modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { NavigationComponent } from './components/common/navigation/navigation.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
